3a0adbe7ce37f88ecc003f6a9301b9d344275372
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2025-04-08 06:08:22 +0100

    all days update

--- JourneyToAtlantis2025/qall.py
+++ JourneyToAtlantis2025/qall.py
@@ -3,9 +3,9 @@ from time import time
 overall_start = time()
 
 for day in range(1, 19):
-    print(f"Day {day}")
+    day_start = time()
     exec(open(f"./JourneyToAtlantis2025/q{day:02d}.py").read())
-    print(f"running time so far: {time() - overall_start:.3f}s")
+    print(f"Day {day}: {time() - day_start:.3f}s")
     print()
 
 print(f"total running time: {time() - overall_start:.3f}s")
